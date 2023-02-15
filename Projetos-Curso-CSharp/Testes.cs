2062a3c6f20c6cc64918ec629ed701f838854b83
uniedit <buraconegroquasar@gmail.com>
2023-02-15 19:32:49 -0300

    Operadores

--- Projetos-Curso-CSharp/Testes.cs
+++ Projetos-Curso-CSharp/Testes.cs
@@ -10,8 +10,7 @@ namespace Projetos_Curso_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Oi");
-
+            Console.WriteLine("Hi");
         }
     }
 }
