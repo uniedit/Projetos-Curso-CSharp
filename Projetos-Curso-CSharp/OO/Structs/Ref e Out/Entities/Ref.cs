f2ffcec0d3bc6ca84ede0ab1a25c2f596abd3ecf
uniedit <buraconegroquasar@gmail.com>
2023-05-06 16:06:33 -0300

    Ref

---
+++ Projetos-Curso-CSharp/OO/Structs/Ref e Out/Entities/Ref.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projetos_Curso_CSharp.OO.Structs.Ref_e_Out.Entities {
+    class Ref {
+
+        public static void Triplo(ref int x) {  // Operação usando um atributo x * 3
+            x = x * 3;
+        }
+
+    }
+}
---
+++ Projetos-Curso-CSharp/OO/Structs/Ref e Out/Ref.cs
@@ -0,0 +1,20 @@
+﻿using Projetos_Curso_CSharp.OO.Structs.Ref_e_Out.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projetos_Curso_CSharp.OO.Structs.Ref_e_Out {
+    public class Classe {
+        internal class Class {
+            public void Method() {
+
+                int a = 5;
+                Ref.Triplo(ref a);  // Referncia "a" apontando para "x" fazendo a operação em "Ref.Triplo"
+                Console.WriteLine(a);
+
+            }
+        }
+    }
+}
