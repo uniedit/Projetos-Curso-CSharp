7c64b0dd5ad0bc9e6e1cc6673f271dc6862eea42
uniedit <eunaoodeiorato@gmail.com>
2025-09-30 19:24:43 -0300

    Herança yupppies
    🐕

---
+++ Projetos-Curso-CSharp/Myo/OO/Herança/Account.cs
@@ -0,0 +1,29 @@
+﻿namespace Projetos_Curso_CSharp.Myo.OO.Herança {
+    class Account {
+
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        // Caso do modificador de acesso "protected" faz com que as subclasses possam
+        // acessar-la, porém apenas em subclasses, não em classes fora desse escopo.
+        public double Balance { get; protected set; }
+
+        public Account() {
+        }
+
+        public Account(int number, string holder, double balance) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount) {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+
+
+    }
+}
---
+++ Projetos-Curso-CSharp/Myo/OO/Herança/BusinessAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace Projetos_Curso_CSharp.Myo.OO.Herança {
+    // Usando " : " para referenciar a superclasse
+    class BusinessAccount : Account {
+
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() {
+        }
+
+        /* 
+            * Usa-se " : base " para se referenciar um construtor de outra classe os metodos que 
+            * devem ser herdados da superclasse para essa subclasse.
+        */
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit)
+            : base(number, holder, balance) {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount) {
+            if (amount <= LoanLimit) {
+                Balance += amount;
+            }
+        }
+
+    }
+}
