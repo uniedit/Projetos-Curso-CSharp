c9eb2ec5dd13fa427132a68e8c2b292882bb4cc0
uniedit <eunaoodeiorato@gmail.com>
2025-10-22 21:25:34 -0300

    Some Lambdassss
    Lambidona

---
+++ Projetos-Curso-CSharp/Myo/Funcional/Linq/Demo_2/Entities/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace Projetos_Curso_CSharp.Myo.Funcional.Linq.Demo_2.Entities {
+    class Category {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
---
+++ Projetos-Curso-CSharp/Myo/Funcional/Linq/Demo_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using Projetos_Curso_CSharp.Myo.Funcional.Linq.Demo_2.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+namespace Projetos_Curso_CSharp.Myo.Funcional.Linq.Demo_2 {
+    class Program {
+
+        static void Print<Generic>(string message, IEnumerable<Generic> collection) {
+            Console.WriteLine(message);
+            foreach (Generic obj in collection) {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+        }
+
+        public void Main(string[] args) {
+
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Eletronics", Tier = 1 };
+
+            List<Product> products = new List<Product>() {
+                new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2 },
+                new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1 },
+                new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3 },
+                new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2 },
+                new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1 },
+                new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2 },
+                new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3 },
+                new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3 },
+                new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2 },
+                new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3 },
+                new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
+            };
+
+            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+
+            Print("Tier 1 and price < 900:", r1);
+
+
+
+
+
+
+
+        }
+    }
+}
